da7ef8e2c1522b14379cc256cbdd9d2b13ea596a
Burbage Stacey <rstacey06@gmail.com>
2019-12-02 01:19:09 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,34 @@
+class Genre 
+
+  extend Concerns::Findable
+  
+  attr_accessor :name
+  attr_reader :songs
+  
+  @@all = []
+  
+  def initialize (name)
+    @name = name 
+    @songs = []
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def self.destroy_all
+     Genre.all.clear 
+  end
+   
+  def save 
+    @@all << self
+  end
+  
+  def self.create(name)
+    self.new(name).tap {|genre| genre.save}
+  end
+  
+   def artists
+    songs.collect{ |s| s.artist }.uniq
+  end
+end
